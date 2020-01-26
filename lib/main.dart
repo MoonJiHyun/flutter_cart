9a4d8164488aef6d14fce614f9da88bd94bfd4ad
MoonJiHyun <boo74153@gmail.com>
2020-01-26 16:18:45 +0900

    flutter_bloc라이브러리를 사용한 Bloc 패턴

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cart/bloc/cart_bloc.dart';
+import 'catalog.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final _cartBloc = CartBloc();
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: _cartBloc,
+      child: BlocBuilder(
+        bloc: _cartBloc,
+        builder: (BuildContext context, List state) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue
+            ),
+            home: LoginScreen()
+          );
+        })
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome')
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Enter'),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Catalog()));
+          },
+        ),
+      )
+    );
+  }
+}
